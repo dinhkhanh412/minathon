b309de4317c00d98962fd2e2efb4104d3d72bb7f
Khanh Nguyen <dinhkhanh412@gmail.com>
2021-03-20 00:44:23 +0700

    Add Register

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -3,6 +3,8 @@ import 'package:minathon/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
+  static Route<Object> idScreen;
+
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
